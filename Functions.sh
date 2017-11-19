c4b300035dc81874071a61cad08d0a949a4fbe84
harish1996 <harishganesan96@gmail.com>
2017-11-20 00:58:49 +0530

    Added comments

--- Functions.sh
+++ Functions.sh
@@ -1,15 +1,58 @@
 #! /bin/bash
 
+# Usage : colors @red @green @blue @text
+# @red => 0 to 255 , Red component of the colour
+# @green => 0 to 255 , Green component of the colour
+# @blue => 0 to 255 , Blue component of the colour
+# @text => Some text wrapped in Double quotes . Text to be coloured .
+# 
+# This function generates two variables which has the coloured version of the text either in the foreground or in the background .
+# For colour to be in the Foreground , use $fg
+# For colour to be in the Background , use $bg
+#
+# Example : For colouring text "Red" coloured in red ( 255,0,0 )
+# 
+# $ colors 255 0 0 "Red"
+# $ echo -e $fg
+#
+# Example : For colouring background of text "Hello" in blue ( 0,0,255 )
+#
+# $ colors 0 0 255 "Hello"
+# $ echo -e $bg
+#
+# Note : It is important to use 'echo -e' or 'printf' inorder to print these variables , since most others dont parse escape sequences .
+
 function colors
 {
 	bg="\e[48;2;$1;$2;$3m$4\e[0m"
 	fg="\e[38;2;$1;$2;$3m$4\e[0m"
 }
 
+
+# Usage : comb_colors @red1 @green1 @blue1 @red2 @green2 @blue2 @text
+# @red1 , @green1 , @blue1 => Colour components of the background colour
+# @red2 , @green2 , @blue2 => Colour components of the foreground colour
+# @text => Text to be coloured
+#
+# This function generates a variable ' $text '  with background and foreground colours as mentioned , with the text.
+#
+# Example : For colouring background in green ( 0,0,255 ) and foreground in red ( 255,0,0 ) with the text "Hello World "
+# $ comb_colors 0 0 255 255 0 0 "Hello World"
+# $ echo -e $text
+# 
+# Note : It is important to use 'echo -e' or 'printf' inorder to print these variables , since most others dont parse escape sequences .
+
 function comb_colors
 {
 	text="\e[48;2;$1;$2;$3m\e[38;2;$4;$5;$6m$7\e[0m"
 }
+
+
+
+
+# Usage : flag
+# Prints an Indian Flag
+
 function flag
 {
 	comb_colors 255 153 51 255 153 51 "indiaflag"
@@ -23,19 +66,15 @@ function flag
 	comb_colors 18 136 7 18 136 7 "indiaflag"
 	echo -e "$text"
 }	
-function rand_date
-{
-	#local _i=0
-	#local _a=0
-	#while [ $_i -lt 6 ]
-	#do 
-	#	_a[$_i]=`expr $RANDOM % 255`
-	#	_i=`expr $_i + 1` 
-	#done 
-	#comb_colors ${_a[0]} ${_a[1]} ${_a[2]} ${_a[3]} ${_a[4]} ${_a[5]} "$(date +%T)"
-	#echo -e -n $text
-	rand_color_text -n "$(date +%T)"
-}
+
+
+# Usage : rand_color_text <options> @text
+# @text => Text to be coloured
+#
+# Options :
+# -n 		Prints a text which doesnt terminate at EOT
+#
+# Generates a random coloured text with a suitable background.
 
 function rand_color_text
 {
@@ -60,6 +99,10 @@ function rand_color_text
 	echo -e $opts "$text"
 }
 
+
+# Usage : printfree
+#  Prints out remaining percentage of RAM in a colour coded manner
+
 function printfree
 {
 	GREEN="0 255 0 255 0 0"
@@ -81,6 +124,17 @@ function printfree
 	comb_colors $color "Free:$free%" 
 	echo -e -n "$text"
 }
+
+# Usage : vdownload @Quality @Youtube_Link
+# @Quality => Video Download Quality
+# 		Quality must be anything within 48k|128k|144p|180p|360p|v144p|v240p|v360p|v720p|v1080p
+#		48k,128k -- Audio only
+#		144p,180p,360p,720p -- Audio/Video
+#		v144p,v240p,v360p,v720p,v1080p -- Video only
+# @Youtube_Link => Youtube Link for the video . Use only video links . Playlist links wont work properly . If a video is taken from a playlist , strip the link until there is only watch=??? attribute in it and not contents after &index=....
+#
+# Example : To download iPhone 8 Advertisement in 720p
+# vdownload 720p https://www.youtube.com/watch?v=k0DN-BZrM4o
 
 function vdownload
 {
